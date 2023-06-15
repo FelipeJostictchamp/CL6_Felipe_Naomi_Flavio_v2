79015b2ea22e655505a8ff1b221225590b830442
FelipeJostictchamp <132060696+FelipeJostictchamp@users.noreply.github.com>
2023-06-16 00:33:51 +0200

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,69 @@
+// (1) Variablen initialisieren
+const formContainer = document.getElementById("formContainer");
+const thankYouContainer = document.getElementById("thankYouContainer");
+const submitButton = document.getElementById("submit");
+submitButton.disabled = true;
+
+const emailField = document.getElementById("email");
+const nameField = document.getElementById("name");
+const vornameField = document.getElementById("vorname");
+
+document.querySelector("form").addEventListener("submit", function(event) {
+  event.preventDefault();
+  onClickSubmit();
+});
+
+// (2) Interaktionen festlegen
+emailField.addEventListener("keyup", () => {
+  onChangeEmailField();
+});
+nameField.addEventListener("keyup", () => {
+  onChangeEmailField();
+});
+vornameField.addEventListener("keyup", () => {
+  onChangeEmailField();
+});
+submitButton.addEventListener("click", () => {
+  onClickSubmit();
+});
+
+const isValidEmail = (email) => {
+  // Einfache Überprüfung der E-Mail-Adresse mit einem regulären Ausdruck
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+};
+
+const onClickSubmit = () => {
+  // Überprüfen der Felder nach dem Klick auf "Senden"
+  if (emailField.value === '' || nameField.value === '' || vornameField.value === '') {
+    alert('Bitte füllen Sie alle Pflichtfelder aus');
+  } else if (nameField.value.length < 2) {
+    alert('Der Name muss mindestens 2 Buchstaben enthalten');
+  } else if (!isValidEmail(emailField.value)) {
+    alert('Bitte geben Sie eine gültige E-Mail-Adresse ein');
+  } else { 
+    // Daten aus dem Formular für die Datenbank bereitstellen
+    const data = {
+      group: "cl6", // SQL Gruppen Namen
+      pw: "b900e1aa", // SQL Passwort
+      tableName: "user", // Name der Tabelle in der SQL Datenbank
+      columns: {
+        email: emailField.value,
+        name: nameField.value,
+        vorname: vornameField.value,
+      },
+    };
+  
+    // Speichert die Daten in der Datenbank
+    databaseClient.insertInto(data)
+      .then(() => {
+        // Nach dem Speichern verschwindet das Formular, eine Dankeschön-Nachricht erscheint
+        formContainer.style.display = "none";
+        thankYouContainer.style.display = "block";
+      })
+      .catch(() => {
+        // Fehler beim Speichern in der Datenbank
+        alert('Ein Fehler ist aufgetreten. Bitte versuchen Sie es später erneut.');
+      });
+  }
+};   
